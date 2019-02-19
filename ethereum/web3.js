d50df44d747435095baab5a48798238241c077de
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-19 16:48:10 +0300

    Update web3 to handle processes in the browser or the server

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,7 +1,20 @@
 import Web3 from 'web3'
+const keys = require('../keys')
 
 // Create a new instance of we3 which uses the provider automatically injected to the browser by MetaMask
-const web3 = new Web3(window.web3.currentProvider)
+let web3
+
+// If our code will run in the browser and MetaMask is available
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+    // We are in the browser and MetaMask is running
+    web3 = new Web3(window.web3.currentProvider)
+} else {
+    // We are in the server *OR* the user is not running MetaMask
+    const provider = new Web3.providers.HttpProvider(
+        keys.infuraRinkebyURL
+    )
+    web3 = new Web3(provider)
+}
 
 // Export the instance
 export default web3
