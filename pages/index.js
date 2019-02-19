a54d2f60a3db98106b0bff635b65e8a7e59afd85
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-19 17:52:46 +0300

    Add open campaigns label

--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ class CampaignIndex extends Component {
                     rel="stylesheet"
                     href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
                 />
+                <h3>Open Campaigns</h3>
                 {this.renderCampaigns()}
                 <Button
                     content= 'Create Campaign'
