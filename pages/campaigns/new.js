7d872054414721501207c5052d9de1204d7d9c2c
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-02-19 19:11:57 +0300

    Add a new page for creating a campaign

---
+++ pages/campaigns/new.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Layout from '../../components/Layout'
+
+class CampaignNew extends Component {
+    
+
+    // -- Render --
+    render() {
+        return(
+            <Layout>
+                <h1>New Campaign!</h1>
+            </Layout>
+        )
+    }
+}
+
+export default CampaignNew
