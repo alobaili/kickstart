12b8628e66b4df81fcfc09f8fa66c80e6053a1fa
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-03-06 20:13:13 +0300

    Setup routes.js file
    A required file by next-routes module

---
+++ routes.js
@@ -0,0 +1,3 @@
+const routes = require('next-routes')() // The second set of () means that the require call returns a function, and that function will run immidiately after the require call.
+
+module.exports = routes
