28906e4636aefb892a31f6063adfb66a4927cac3
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-03-06 21:38:52 +0300

    Add a route rule for show.js

--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
 const routes = require('next-routes')() // The second set of () means that the require call returns a function, and that function will run immidiately after the require call.
 
+routes
+    .add('/campaigns/new', '/campaigns/new')
+    .add('/campaigns/:address', '/campaigns/show') // the ':' means that 'address' will be a variable which is passed to the component
 module.exports = routes
