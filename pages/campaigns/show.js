4441a48671ad1c83d840cf94ac273068bb2f1cf2
Abdulaziz Alobaili <dr.alobaili@gmail.com>
2019-03-08 03:22:33 +0300

    Add contribute form

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import Layout from '../../components/Layout'
 import Campaign from '../../ethereum/campaign'
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import web3 from '../../ethereum/web3'
+import ContributeForm from '../../components/ContributeForm'
 
 class CampaignShow extends Component {
 
@@ -18,6 +19,7 @@ class CampaignShow extends Component {
 
         // the return statement required by the getInitialProps function. We set props for our component from the summary call.
         return {
+            address: props.query.address,
             minimumContribution: summary[0],
             balance: summary[1],
             requestsCount: summary[2],
@@ -75,7 +77,15 @@ class CampaignShow extends Component {
         return (
             <Layout>
                 <h3>Campaign Show</h3>
-                {this.renderCards()}
+                <Grid>
+                    <Grid.Column width={10}>
+                        {this.renderCards()}
+                    </Grid.Column>
+
+                    <Grid.Column width={6}>
+                        <ContributeForm address={this.props.address}/>
+                    </Grid.Column>
+                </Grid>
             </Layout>
         )
     }
